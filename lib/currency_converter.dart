6991e7f1b0d392718c01864613fcaaa2d2b2d18a
Allen Jude <ajallen142001@gamil.com>
2025-06-19 21:31:04 +0530

    Commit

---
+++ lib/currency_converter.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverter extends StatelessWidget{
+  const CurrencyConverter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromRGBO(56, 100, 221, 0.5),
+                    strokeAlign: BorderSide.strokeAlignCenter,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                );
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(0, 0, 0, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Currency Converter",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(56, 100, 221, 1)
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                style: TextStyle(
+                  color: Color.fromARGB(255, 217, 255, 0)
+                ),
+                decoration: InputDecoration(
+                  
+                  hintText: "amount in USD",
+                  prefixIcon: Icon(
+                    Icons.monetization_on_sharp, 
+                    color:Color.fromARGB(255, 255, 255, 255),
+                  ), 
+                  filled: true,
+                  fillColor: const Color.fromARGB(99, 78, 67, 124),
+                  focusedBorder: border, 
+                  enabledBorder: border,
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ), 
+            ),
+
+            TextButton(
+              onPressed: (){}, 
+              style: ButtonStyle(
+                foregroundColor: MaterialStatePropertyAll(Colors.white)
+              ),
+              child: Text("Convert"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
